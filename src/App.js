8efb879d3772c5da9de44a5a87115337fcc95d47
Vijay Rasipogula <rvkfriendz@gmail.com>
2022-08-11 00:09:39 +0530

    IQG

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <button>Heading1</button>
+       <p>The Bangalore city is very cool when campare to delhi</p>       
+      </div>
+    );
+  }
+}
+
+export default App;
